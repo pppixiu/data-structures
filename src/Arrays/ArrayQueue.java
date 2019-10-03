6a4912362e71084120e388014a04b73bda5fe895
sensu <wjs11234567@163.com>
2019-10-03 16:00:37 +0800

    ArrayQueue

---
+++ src/Arrays/ArrayQueue.java
@@ -0,0 +1,48 @@
+package Arrays;
+
+/**
+ * @ClassName ArrayQueue
+ * @Author sensu
+ * @Date 2019/10/3 15:55
+ **/
+public class ArrayQueue<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public ArrayQueue(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue(){
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+}
---
+++ src/Arrays/Queue.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+/**
+ * @ClassName Queue
+ * @Author sensu
+ * @Date 2019/10/3 15:52
+ **/
+public interface Queue<E> {
+    /**
+     * 返回队列大小
+     *
+     * @return
+     */
+    int getSize();
+
+    /**
+     * 判断队列是否为空
+     *
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 入队操作
+     *
+     * @param e
+     */
+    void enqueue(E e);
+
+    /**
+     * 出队操作
+     *
+     * @return
+     */
+    E dequeue();
+
+    /**
+     * 查看队列头部的元素
+     *
+     * @return
+     */
+    E getFront();
+
+}
